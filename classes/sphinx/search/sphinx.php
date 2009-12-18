39e265e1c064d0c4c341925b01576ee1475eb0a2
Kyle Wascher <kyle@zoxive.com>
2009-12-17 23:12:50 -0500

    Added ability to start/stop/restart/index. Also made the search Iteratable and countable.
    Need to work on config settings per index. And search querys to sphinx.

--- classes/sphinx/search/sphinx.php
+++ classes/sphinx/search/sphinx.php
@@ -1,15 +1,57 @@
 <?php
 
-class Sphinx_Search_Sphinx
+class Sphinx_Search_Sphinx implements Iterator, Countable
 {
 
     protected $model = NULL;
-
     public $query = NULL;
+
+    protected $search = NULL;
 
     static public function factory($model)
     {
         return new self($model);
+    }
+
+    static protected function run($cmd, $push = FALSE)
+    {
+        return $push? `{$cmd} > /dev/null 2>&1` : `{$cmd}`;
+    }
+
+    static public function restart()
+    {
+        $cmd1 = Sphinx_Search::stop();
+        $cmd2 = Sphinx_Search::start();
+        return $cmd1.PHP_EOL.$cmd2;
+    }
+
+    static public function stop()
+    {
+        $bin = Kohana::config('sphinx.default.bin'); 
+        $config = Kohana::config('sphinx.default.conf');
+        return Sphinx_Search::run($bin.'/searchd --config '.$config.' --stop');
+    }
+
+    static public function start()
+    {
+        $bin = Kohana::config('sphinx.default.bin'); 
+        $config = Kohana::config('sphinx.default.conf');
+        return Sphinx_Search::run($bin.'/searchd --config '.$config);
+    }
+
+    static public function stats()
+    {
+        $bin = Kohana::config('sphinx.default.bin'); 
+        $config = Kohana::config('sphinx.default.conf');
+        return Sphinx_Search::run($bin.'/searchd --config '.$config.' --status');
+    }
+
+    static public function index($index = null, $push = FALSE)
+    {
+        $bin = Kohana::config('sphinx.default.bin'); 
+        $config = Kohana::config('sphinx.default.conf');
+        $index = is_null($index)? '--all' : $index;
+        return Sphinx_Search::run($bin.'/indexer '.$index.' --config '.$config.' --rotate', $push);
     }
 
     public function __construct($model, $config = 'default')
@@ -25,6 +67,7 @@ class Sphinx_Search_Sphinx
         $this->config = Kohana::config('sphinx.'.$config);
 
         $this->load_model_config();
+
     }
 
     protected function load_model_config()
@@ -32,43 +75,82 @@ class Sphinx_Search_Sphinx
         $this->model_config = $this->model->_sphinx_index();
     }
 
-    public function search()
+    public function count()
     {
-        $sl = new SphinxClient(); 
-        $sl->SetMatchMode(SPH_MATCH_EXTENDED2);
-        $sl->SetLimits(0, 100);
-        $sl->SetSortMode(SPH_SORT_EXTENDED, '@relevance DESC');
-        
-        $result = $sl->Query($this->query, $this->model_config->index);
-        
-        if ($result && isset($result['matches']))
-        {
-            $docids = array_keys($result['matches']);
-        }
-        else
-        {
-            var_dump($sl->GetLastError());
-            var_dump($sl->GetLastWarning());
-            return array();
-        }
-
-        $query = DB::select()
-            ->where($this->model->pk(), 'IN', $docids)
-            ->order_by(new Database_Expression('FIELD('.$this->model->pk().', '.implode(',', $docids).')'));
-
-        return $this->model->load($query, null);
+        return count($this->search());
+    }
+    public function reset()
+    {
+        $this->search = NULL;
+    }
+    public function rewind()
+    {
+        return $this->search()? $this->search()->rewind() : FALSE;
+    }
+    public function current()
+    {
+        return $this->search()->current();
+    }
+    public function key()
+    {
+        return $this->search()->key();
+    }
+    public function next()
+    {
+        return $this->search()->next();
+    }
+    public function valid()
+    {
+        return $this->search()? $this->search()->valid() : FALSE;
     }
 
-    public function index($verbose = FALSE)
+    public function search()
+    {
+        if (is_null($this->search))
+        {
+            $sl = new SphinxClient(); 
+            $sl->SetMatchMode(SPH_MATCH_EXTENDED2);
+            $sl->SetLimits(0, 100);
+            $sl->SetSortMode(SPH_SORT_EXTENDED, '@relevance DESC');
+            
+            $result = $sl->Query($this->query, $this->model_config->index);
+            
+            if ($result && isset($result['matches']))
+            {
+                $docids = array_keys($result['matches']);
+
+                $query = DB::select()
+                    ->where($this->model->pk(), 'IN', $docids)
+                    ->order_by(new Database_Expression('FIELD(`'.$this->model->pk().'`, '.implode(',', $docids).')'));
+
+                $this->search = $this->model->load($query, null);
+            }
+            else
+            {
+                var_dump($result);
+                $this->search = array();
+            }
+        }
+        return $this->search;
+    }
+
+    public function run_index($push = FALSE)
+    {
+        $this->mk_index();
+
+        return Sphinx_Search::index($this->model_config->index, $push);
+    }
+
+    public function mk_index()
     {
         $docroot = DOCROOT;
 
         $file = "
-source {$this->model_config->index}_src
-{
-    type    = mysql
-";
-    
+        source {$this->model_config->index}_src
+        {
+            type    = mysql
+        ";
+
 
         foreach($this->config['database'] as $key => $variable)
         {
@@ -83,37 +165,29 @@ source {$this->model_config->index}_src
         }
 
         $file.="
-}
+        }
 
-index {$this->model_config->index}
-{
-    source = {$this->model_config->index}_src
+        index {$this->model_config->index}
+        {
+            source = {$this->model_config->index}_src
 
-    path = {$docroot}{$this->config['data_folder']}/data/{$this->model_config->index}
+            path = {$docroot}{$this->config['data_folder']}/data/{$this->model_config->index}
 
-    docinfo = extern
+            docinfo = extern
 
-    mlock = 0
+            mlock = 0
 
-    min_stemming_len = 4
+            min_stemming_len = 4
 
-    min_word_len = 1
-";
+            min_word_len = 1
+        ";
 
 
         $file.="
-}
-";
+        }
+        ";
 
         $filename = DOCROOT.$this->config['data_folder'].'/sphinx_'.$this->model_config->index.'.conf';
         file_put_contents($filename, $file);
-
-        $report = `{$this->config['bin']}/indexer {$this->model_config->index} --config {$docroot}modules/sphinx/classes/sphinx.php --rotate`;
-
-        if ( $verbose ) {
-            echo '<pre>';
-            echo $report;
-            echo '</pre>';
-        }
     }
 }
--- config/sphinx.php
+++ config/sphinx.php
@@ -6,6 +6,7 @@ return array(
         'bin'       =>  '/usr/lib/sphinx/bin',
         'data_folder'   =>  'application/cache/sphinx',
         'core_file'     =>  'application/core_sphinx.conf',
+        'conf'        =>  DOCROOT.'modules/sphinx/classes/sphinx.php',
         'database'  =>  array(
             'sql_host'  =>  Kohana::config('database.default.connection.hostname'),
             'sql_user'  =>  Kohana::config('database.default.connection.username'),
